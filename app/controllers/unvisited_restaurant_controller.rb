34544227810b04a9f67e0560f26940fbcc92cf9e
github username <github email address>
2018-08-16 11:31:23 +0200

    added post for new unvisited restaurant

--- app/controllers/unvisited_restaurant_controller.rb
+++ app/controllers/unvisited_restaurant_controller.rb
@@ -1,15 +1,23 @@
+require 'rack-flash'
+
 class UnvisitedController < ApplicationController
+  use Rack::Flash
 
   get '/unvisted/new' do
     if logged_in?
       erb :'unvisted_restaurants/new'
     else
-      '/login'
+      redirect to '/login'
     end
   end
 
   post '/unvisited' do
+    @restaurant = UnvisitedRestaurant.create(name: params[:name], location: params[:location])
+    @restaurant.user_id = current_user.id
+    @restaurant.save
 
+    flash[:message] = "The new restaurant has been added to your wish list."
+    redirect to "/users/:#{@restaurant.user_id}"
   end
 
 end
